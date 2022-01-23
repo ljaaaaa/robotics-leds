f786b49475c3c98be53c8f53dfab279d4a7c157d
LiljaK <lilja.maria.kiiski@gmail.com>
2022-01-23 15:22:17 -0800

    Test commit on VSCode

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//Tiny change
+
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
