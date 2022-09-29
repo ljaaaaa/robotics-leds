2897da7da20d5335c14253882cc2a30f13d06314
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-09-28 22:52:56 -0700

    Added code to test out friday with leds

--- Arduino/Main/Main.ino
+++ Arduino/Main/Main.ino
@@ -1,17 +1,25 @@
 #include "FastLED.h"
 
 //Pins
-#define DATA_PIN 8
+#define DATA_PIN 7
 
 //Leds
-#define NUM_LEDS 56
+#define NUM_LEDS 100
 CRGB leds[NUM_LEDS];
 const uint8_t brightness = 60;
 
 void setup(){
   Serial.begin(9600);
 
+  //Mine
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); //for GRB LEDs
+  
+  //Maybe MVRTs?
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  
+  //Also maybe
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
   FastLED.setBrightness(brightness);
 
   fading_rainbow();
